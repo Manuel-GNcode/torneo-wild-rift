86b7bcfaad0448b9d57f2e4e8df4dfecb76c4ace
GNcode <gngalarga027@gmail.com>
2025-11-07 16:17:32 -0500

    Add player data, main functionality, styles, and Vite configuration
    - Created data.js to store player information.
    - Implemented main.js to display top player and render player table.
    - Added styles in style.css using Tailwind CSS and custom fonts.
    - Configured Vite with Tailwind CSS plugin for build process.

---
+++ src/main.js
@@ -0,0 +1,39 @@
+import { dataPlayers } from "./data";
+
+const $topPlayerImg = document.getElementById('top-player-img');
+const $topPlayerName = document.getElementById('top-player-name');
+const $topPlayerPuntos = document.getElementById('top-player-puntos');
+const $topPlayerKills = document.getElementById('top-player-kills');
+const $topPlayerDefeat = document.getElementById('top-player-defeat');
+
+const $tableBody = document.getElementById('table-body');
+
+document.addEventListener('DOMContentLoaded', ()=>{
+  const topPlayer = dataPlayers[0];
+
+  $topPlayerImg.src = topPlayer.catImg;
+  $topPlayerName.innerText = topPlayer.name;
+
+  $topPlayerPuntos.innerText = topPlayer.puntos;
+  $topPlayerKills.innerText = topPlayer.kills;
+  $topPlayerDefeat.innerText = topPlayer.defeat;
+
+  renderPlayersInTable(dataPlayers);
+})
+
+const renderPlayersInTable = (arrayPlayers)=>{
+  for (let index = 0; index < arrayPlayers.length; index++) {
+    const player = arrayPlayers[index];
+    
+    const trHTML = `
+    <tr>
+      <td class="table-rank"><p class="rounded-sm">${index + 1}</p></td>
+      <td class="table-jugador">${player.name}</td>
+      <td class="table-puntos">${player.puntos}</td>
+      <td class="table-kills">${player.kills}</td>
+    </tr>
+    `
+
+    $tableBody.innerHTML += trHTML;
+  }
+}
